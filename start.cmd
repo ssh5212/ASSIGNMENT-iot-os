c636de5e2af96af2b9849f8c8d568ec992ce4c61
AngelPlayer <ssh5212@naver.com>
2021-12-11 03:56:45 +0900

    Default

---
+++ start.cmd
@@ -0,0 +1,14 @@
+@echo off
+cd D:\Study\hello\IoT_OS\make_data
+python Add_Yesterday.py
+timeout 30
+
+cd D:\Study\hello\IoT_OS
+python train.py
+timeout 1000
+
+python test.py
+
+git add *
+git commit -m "Data Update"
+git push
