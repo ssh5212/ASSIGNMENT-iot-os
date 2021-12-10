4606b8ea851dc44c02f297bee8171c40946b8217
AngelPlayer <ssh5212@naver.com>
2021-12-11 04:59:03 +0900

    path change

--- make_data/Data_Process.py
+++ make_data/Data_Process.py
@@ -1,7 +1,7 @@
 import collections
 import pandas as pd
 
-data = pd.read_csv('D:\Study\hello\IoT\make_data\mergin.csv', encoding='cp949') # 파일 읽기
+data = pd.read_csv('D:\Study\hello\IoT_OS\make_data\mergin.csv', encoding='cp949') # 파일 읽기
 data = data.drop(['지점', '지점명'], axis=1) # 사용하지 않는 row 삭제
 data['강수량(mm)'] = data['강수량(mm)'].fillna(0) # 강수량 누락 값을 0으로 채움
 data = data.fillna(method='bfill') # 바로 앞 데이터로 채움
